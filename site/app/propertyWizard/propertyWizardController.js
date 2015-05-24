eaf4f81aaec705eb032f7261bfcfb16ced3dbe63
alex_viderman <Alex1212>
2015-05-24 16:45:14 -0700

    0.2.9 wizard updates. masks. google places

--- site/app/propertyWizard/propertyWizardController.js
+++ site/app/propertyWizard/propertyWizardController.js
@@ -1,6 +1,8 @@
 'use strict';
 define([
     'app',
+    '../../components/inputmask/module.js',
+    'async!//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places'
 ], function (app) {
      app.controller
         ('propertyWizardController', ['$scope', '$modalInstance', 'id', 'isComp', 'ngProgress', '$rootScope','toastr', '$location', '$propertyService', function ($scope, $modalInstance, id, isComp, ngProgress, $rootScope, toastr, $location, $propertyService) {
@@ -13,16 +15,25 @@ define([
                 $modalInstance.dismiss('cancel');
             };
 
-            if (!id && isComp) {
-                $scope.title = "Create Comp Property"
-            } else {
-                $scope.title = "Create Subject Property"
+            $scope.getTitle = function() {
+                var title = "";
+                if (!id && isComp) {
+                    title = "Create Comp"
+                } else {
+                    title = "Create Property"
+                }
+
+                if ($scope.property.name) {
+                    title += ": " + $scope.property.name;
+                }
+
+                return title;
             }
 
 
             $scope.steps = [
                 {label:'Property Info', template: 'propertyInfo.html'},
-                {label:'Amenities', template: 'amenities.html'},
+                {label:'Property Amenities', template: 'amenities.html'},
                 {label:'Fees & Deposits', template: 'feesDeposits.html'},
                 {label:'Floor Plans', template: 'floorPlans.html'},
                 {label:'Notes', template: 'notes.html'},
@@ -44,5 +55,256 @@ define([
 
             $scope.changeStep(0);
             $scope.property = { }
+
+            $scope.constructionTypes = ['Garden','Highrise','Midrise','Platform','Wrap']
+            $scope.states = [
+                {
+                    "name": "Alabama",
+                    "abbreviation": "AL"
+                },
+                {
+                    "name": "Alaska",
+                    "abbreviation": "AK"
+                },
+                {
+                    "name": "Arizona",
+                    "abbreviation": "AZ"
+                },
+                {
+                    "name": "Arkansas",
+                    "abbreviation": "AR"
+                },
+                {
+                    "name": "California",
+                    "abbreviation": "CA"
+                },
+                {
+                    "name": "Colorado",
+                    "abbreviation": "CO"
+                },
+                {
+                    "name": "Connecticut",
+                    "abbreviation": "CT"
+                },
+                {
+                    "name": "Delaware",
+                    "abbreviation": "DE"
+                },
+                {
+                    "name": "District Of Columbia",
+                    "abbreviation": "DC"
+                },
+                {
+                    "name": "Florida",
+                    "abbreviation": "FL"
+                },
+                {
+                    "name": "Georgia",
+                    "abbreviation": "GA"
+                },
+                {
+                    "name": "Hawaii",
+                    "abbreviation": "HI"
+                },
+                {
+                    "name": "Idaho",
+                    "abbreviation": "ID"
+                },
+                {
+                    "name": "Illinois",
+                    "abbreviation": "IL"
+                },
+                {
+                    "name": "Indiana",
+                    "abbreviation": "IN"
+                },
+                {
+                    "name": "Iowa",
+                    "abbreviation": "IA"
+                },
+                {
+                    "name": "Kansas",
+                    "abbreviation": "KS"
+                },
+                {
+                    "name": "Kentucky",
+                    "abbreviation": "KY"
+                },
+                {
+                    "name": "Louisiana",
+                    "abbreviation": "LA"
+                },
+                {
+                    "name": "Maine",
+                    "abbreviation": "ME"
+                },
+                {
+                    "name": "Maryland",
+                    "abbreviation": "MD"
+                },
+                {
+                    "name": "Massachusetts",
+                    "abbreviation": "MA"
+                },
+                {
+                    "name": "Michigan",
+                    "abbreviation": "MI"
+                },
+                {
+                    "name": "Minnesota",
+                    "abbreviation": "MN"
+                },
+                {
+                    "name": "Mississippi",
+                    "abbreviation": "MS"
+                },
+                {
+                    "name": "Missouri",
+                    "abbreviation": "MO"
+                },
+                {
+                    "name": "Montana",
+                    "abbreviation": "MT"
+                },
+                {
+                    "name": "Nebraska",
+                    "abbreviation": "NE"
+                },
+                {
+                    "name": "Nevada",
+                    "abbreviation": "NV"
+                },
+                {
+                    "name": "New Hampshire",
+                    "abbreviation": "NH"
+                },
+                {
+                    "name": "New Jersey",
+                    "abbreviation": "NJ"
+                },
+                {
+                    "name": "New Mexico",
+                    "abbreviation": "NM"
+                },
+                {
+                    "name": "New York",
+                    "abbreviation": "NY"
+                },
+                {
+                    "name": "North Carolina",
+                    "abbreviation": "NC"
+                },
+                {
+                    "name": "North Dakota",
+                    "abbreviation": "ND"
+                },
+                {
+                    "name": "Ohio",
+                    "abbreviation": "OH"
+                },
+                {
+                    "name": "Oklahoma",
+                    "abbreviation": "OK"
+                },
+                {
+                    "name": "Oregon",
+                    "abbreviation": "OR"
+                },
+                {
+                    "name": "Palau",
+                    "abbreviation": "PW"
+                },
+                {
+                    "name": "Pennsylvania",
+                    "abbreviation": "PA"
+                },
+                {
+                    "name": "Rhode Island",
+                    "abbreviation": "RI"
+                },
+                {
+                    "name": "South Carolina",
+                    "abbreviation": "SC"
+                },
+                {
+                    "name": "South Dakota",
+                    "abbreviation": "SD"
+                },
+                {
+                    "name": "Tennessee",
+                    "abbreviation": "TN"
+                },
+                {
+                    "name": "Texas",
+                    "abbreviation": "TX"
+                },
+                {
+                    "name": "Utah",
+                    "abbreviation": "UT"
+                },
+                {
+                    "name": "Vermont",
+                    "abbreviation": "VT"
+                },
+                {
+                    "name": "Virginia",
+                    "abbreviation": "VA"
+                },
+                {
+                    "name": "Washington",
+                    "abbreviation": "WA"
+                },
+                {
+                    "name": "West Virginia",
+                    "abbreviation": "WV"
+                },
+                {
+                    "name": "Wisconsin",
+                    "abbreviation": "WI"
+                },
+                {
+                    "name": "Wyoming",
+                    "abbreviation": "WY"
+                }
+            ];
+
+            window.setTimeout( function() {
+                var autocomplete = new google.maps.places.Autocomplete(
+                    (document.getElementById('autocomplete'))
+                );
+
+                google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                    var place = autocomplete.getPlace();
+                    $scope.property.name = place.name;
+                    $scope.property.phone = place.formatted_phone_number;
+
+                    place.address_components.forEach(function(c) {
+                        switch(c.types[0]) {
+                            case "street_number":
+                                $scope.property.address = c.short_name;
+                                break;
+                            case "route":
+                                $scope.property.address += " " + c.long_name;
+                                break;
+                            case "postal_code":
+                                $scope.property.zip = c.short_name;
+                                break;
+                            case "locality":
+                                $scope.property.city = c.long_name;
+                                break;
+                            case "administrative_area_level_1":
+                                $scope.property.state = c.short_name;
+                                break;
+                        }
+                    })
+
+                    $scope.states.forEach(function(s) {
+                        if (s.abbreviation == $scope.property.state) {
+                            $scope.property.state = s;
+                        }
+                    })
+                });
+            }, 500);
         }]);
+
 });
---
+++ site/components/inputmask/module.js
@@ -0,0 +1,22 @@
+﻿define([
+    'app',
+    '../../components/inputmask/jquery.inputmask.bundle.min.js'
+], function (app) {
+    app.directive('inputMask', function () {
+        return {
+            restrict: 'A',
+            link: function ($scope, element, attr) {
+                $(element).inputmask({
+                    mask:attr.inputMask,
+                    definitions: {
+                        '*': {
+                            validator: "[0-9A-Za-z.!#$%&'*+/=?^_`{|}~\-]",
+                            cardinality: 1,
+                            casing: "lower"
+                        }
+                    }
+                });
+            }
+        };
+    })
+})
