6afaa1214813d6ecdae18cf125425f00dd626c79
alex_viderman <Alex1212>
2015-06-06 18:06:00 -0700

    0.4.8 market sruvey UI

--- site/app/marketSurvey/marketSurveyController.js
+++ site/app/marketSurvey/marketSurveyController.js
@@ -2,6 +2,7 @@
 define([
     'app',
     '../../components/inputmask/module.js',
+    '../../components/ngEnter/module.js',
 ], function (app) {
      app.controller
         ('marketSurveyController', ['$scope', '$modalInstance', 'id', 'ngProgress', '$rootScope','toastr', '$location', '$propertyService', function ($scope, $modalInstance, id, ngProgress, $rootScope, toastr, $location, $propertyService) {
@@ -26,12 +27,77 @@ define([
                 $scope.hasPhone = $scope.property.phone && $scope.property.phone.length > 0;
                 $scope.hasContact = $scope.hasName || $scope.hasEmail || $scope.hasPhone;
 
+                $scope.survey = {}
 
+                $scope.property.floorplans.forEach(function(fp) {
+                    fp.rent = 0;
+                    fp.concessions = 0;
+                })
                 $scope.localLoading = true;
+                window.setTimeout(function() {
+                    var first = $('.survey-values').find('input')[0];
+                    first.focus();
+                    first.select();
+                }, 300);
 
             });
 
+            $scope.updateDone = function(fp, state) {
+                if (typeof fp == 'string') {
+                    switch(fp) {
+                        case "occupancy":
+                            $scope.survey.occupancyupdated = state;
+                            break;
+                        case "traffic":
+                            $scope.survey.trafficupdated = state;
+                            break;
+                        case "leases":
+                            $scope.survey.leasesupdated = state;
+                            break;
 
+                    }
+                }
+                else {
+                    fp.updated = state;
+                }
+
+            }
+
+            $scope.update = function(fp) {
+                $scope.updateDone(fp, true);
+            }
+
+            $scope.undo = function(fp) {
+                $scope.updateDone(fp, false);
+            }
+
+            $scope.next = function(fp, id) {
+                var all = $('.survey-values input');
+
+                if (all.length == 0) {
+                    return;
+                }
+
+                var first = all.first();
+                var current = all.find('#' + id)
+
+                var next = 0;
+                all.each(function(i, v) {
+                    if (v.id == id) {
+                        next = i + 1;
+                    }
+                })
+
+                if (next >= all.length) {
+                    next = 0;
+                }
+                all[next].focus();
+                all[next].select();
+
+                if (id.indexOf("rent") == -1) {
+                    $scope.update(fp)
+                }
+            }
 
 }]);
 
