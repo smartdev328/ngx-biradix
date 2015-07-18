d56b681b5933eb0dbe691099aef618afcf72ae2e
alex_viderman <Alex1212>
2015-07-17 22:18:59 -0700

    0.9.4 finish rankings

--- site/components/reports/propertyRankings.js
+++ site/components/reports/propertyRankings.js
@@ -12,38 +12,107 @@ define([
                 settings: '='
             },
             controller: function ($scope) {
-                $scope.rankings = {}
+
+                $scope.sort = {nersqft:true}
+                $scope.defaultSort = "-nersqft";
+                $scope.orderBy = "-nersqft";
 
                 $scope.report = _.sortByAll($scope.report, ['bedrooms', 'bathrooms'])
-                $scope.report.forEach(function(fp) {
-                    $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms] = $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms] || {};
 
-                    $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].floorplans = $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].floorplans || [];
+                $scope.reload = function() {
+                    $scope.rankings = {}
+                    $scope.report.forEach(function (fp) {
 
-                    var f = {description: fp.description, units : fp.units, sqft: fp.sqft, ner: fp.ner, nersqft: fp.nersqft};
+                        $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms] = $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms] || {};
+
+                        $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].floorplans = $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].floorplans || [];
+
+                        if ($scope.settings.hideUnlinked && fp.excluded) {
+                            $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].excluded = true;
+                        }  else {
+                            var f = {
+                                description: fp.description,
+                                units: fp.units,
+                                sqft: fp.sqft,
+                                ner: fp.ner,
+                                nersqft: fp.nersqft
+                            };
 
 
-                    if ($scope.subject._id == fp.id) {
-                        f.name = $scope.subject.name;
-                    } else {
-                        f.name = _.find($scope.comps, function(x) {
-                            return x.id == fp.id}).name;
+                            if ($scope.subject._id == fp.id) {
+                                f.name = $scope.subject.name;
+                                f.subject = true;
+                            } else {
+                                f.name = _.find($scope.comps, function (x) {
+                                    return x.id == fp.id
+                                }).name;
+                            }
+
+                            $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].floorplans.push(f);
+
+                            $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary = $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary || {};
+
+                            $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.units = ($scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.units || 0) + fp.units;
+
+                            $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.totalsqft = ($scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.totalsqft || 0) + fp.units * fp.sqft;
+
+                            $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.totalner = ($scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.totalner || 0) + fp.units * fp.ner;
+                        }
+                    })
+
+                    for (var fp in $scope.rankings) {
+                        $scope.rankings[fp].summary.sqft = $scope.rankings[fp].summary.totalsqft / $scope.rankings[fp].summary.units;
+                        $scope.rankings[fp].summary.ner = $scope.rankings[fp].summary.totalner / $scope.rankings[fp].summary.units;
+                    }
+                }
+
+                $scope.reload();
+
+                $scope.$on('data.reload', function(event, args) {
+                    $scope.reload();
+                });
+
+                $scope.toggle = function (obj, v, reset) {
+                    var s = obj[v];
+
+                    if (reset) {
+                        for (var i in obj) {
+                            if (i != v) {
+                                delete obj[i];
+                            }
+                        }
                     }
 
-                    $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].floorplans.push(f);
+                    if (s === true) {
+                        obj[v] = false
+                        return;
+                    }
 
-                    $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary = $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary || {};
+                    if (s === false) {
+                        obj[v] = null
+                        return;
+                    }
 
-                    $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.units = ($scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.units || 0) + fp.units;
+                    obj[v] = true;
 
-                    $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.totalsqft = ($scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.totalsqft || 0) + fp.units * fp.sqft;
+                }
 
-                    $scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.totalner = ($scope.rankings[fp.bedrooms + 'x' + fp.bathrooms].summary.totalner || 0) + fp.units * fp.ner;
-                })
+                $scope.toggleSort = function (v) {
+                    $scope.toggle($scope.sort, v, true)
 
-                for (var fp in $scope.rankings) {
-                    $scope.rankings[fp].summary.sqft = $scope.rankings[fp].summary.totalsqft / $scope.rankings[fp].summary.units;
-                    $scope.rankings[fp].summary.ner = $scope.rankings[fp].summary.totalner / $scope.rankings[fp].summary.units;
+                    var s = $scope.sort[v];
+
+                    if (s == null) {
+                        $scope.orderBy = $scope.defaultSort;
+                        return;
+                    }
+
+                    if (s == true) {
+                        $scope.orderBy = "-" + v;
+                    }
+                    else {
+                        $scope.orderBy = v;
+                    }
                 }
 
             },
