a08978da8eb21f11b57636aa96e5c9873aa8946e
alex_viderman <Alex1212>
2015-09-04 20:52:38 -0700

    Change settings to look for mongo hq string as well

--- config/settings.js
+++ config/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     RAYGUN_APIKEY: process.env.RAYGUN_APIKEY || 'XJ1lA7KU3Esr4RvtAiaObQ==',
-    MONGODB_URI: process.env.MONGOLAB_URI || 'mongodb://heroku_app36507393:dfahaf7jb2gkpmcgh538pmj8d8@ds031822.mongolab.com:31822/heroku_app36507393',
+    MONGODB_URI: process.env.MONGOHQ_URL ||process.env.MONGOLAB_URI || 'mongodb://localhost:27017/Biradix',
     SENDGRID_USERNAME : process.env.SENDGRID_USERNAME || 'app36507393@heroku.com',
     SENDGRID_PASSWORD : process.env.SENDGRID_PASSWORD || 'vnlgjsnu0908',
     MODE : process.env.mode || 'development',
