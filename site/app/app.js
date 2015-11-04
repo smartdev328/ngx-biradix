55c456f992fb4e8dabfd5b7210c1c3ac32c154a0
alex_viderman <Alex1212>
2015-11-04 13:05:56 -0700

    1.7.1 RUM RUM RUM

--- site/app/app.js
+++ site/app/app.js
@@ -230,6 +230,32 @@ define([
             $rootScope.$on('$stateChangeStart', function() {
                 $modalStack.dismissAll('cancel');
             });
+
+            var hadRouteChange = false;
+            $rootScope.$on("$routeChangeStart", function() {
+                hadRouteChange = true;
+            });
+            function hook() {
+                if (window.BOOMR && BOOMR.version) {
+                    if (BOOMR.plugins && BOOMR.plugins.Angular) {
+                        BOOMR.plugins.Angular.hook($rootScope, hadRouteChange);
+                    }
+                    return true;
+                }
+            }
+
+            if (!hook()) {
+                if (document.addEventListener) {
+                    document.addEventListener("onBoomerangLoaded", hook);
+                } else if (document.attachEvent) {
+                    document.attachEvent("onpropertychange", function(e) {
+                        e = e || window.event;
+                        if (e && e.propertyName === "onBoomerangLoaded") {
+                            hook();
+                        }
+                    });
+                }
+            }
         }
     ]);
 
