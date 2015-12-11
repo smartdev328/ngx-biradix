c0ea5d1b69bafc8bbd18df7df1824cd916a4cdbf
alex_viderman <Alex1212>
2015-12-10 22:48:55 -0700

    more notifications

--- site/app/updateprofile/updateProfileController.js
+++ site/app/updateprofile/updateProfileController.js
@@ -10,8 +10,6 @@ define([
                 $location.path('/login')
             }
 
-            console.log($stateParams);
-
             if ($stateParams.password === "1") {
                 $('html, body').animate({
                     scrollTop: ($('#passwordPannel').offset().top - 80)
@@ -36,6 +34,34 @@ define([
             $rootScope.sideNav = "UpdateProfile";
 
 
+            $scope.nots = {
+                howOftenOptions: ["Weekly","Monthly"],
+                daysOfWeek: [
+                    {id:0,name:'Sunday'},
+                    {id:1,name:'Monday'},
+                    {id:2,name:'Tuesday'},
+                    {id:3,name:'Wednesday'},
+                    {id:4,name:'Thursday'},
+                    {id:5,name:'Friday'},
+                    {id:6,name:'Saturday'},
+                ],
+
+                daysOfMonth: [
+
+                ],
+            };
+
+            for (var i = 1; i < 32; i++) {
+                var name = "th";
+                if (i == 1 || i == 21 || i == 31) {name = "st"}
+                if (i == 2 || i == 22) {name = "nd"}
+                if (i == 3 || i == 23) {name = "rd"}
+
+                $scope.nots.daysOfMonth.push({id:i,name: i.toString() + name})
+            }
+
+            $scope.nots.daysOfMonth.push({id:"L",name:'Last'});
+
             var unbind = $rootScope.$watch("me", function(x) {
                 if ($rootScope.me) {
                     $scope.user = { first: $rootScope.me.first, last:  $rootScope.me.last, email:  $rootScope.me.email }
@@ -48,6 +74,22 @@ define([
 
                     if ($rootScope.me.bounceReason) {
                         toastr.error('We were unable to deliver email to your email address: <b>' + $rootScope.me.email + '</b>. Please verify your email address and click "Update".');
+                    }
+
+                    $scope.nots.howOften = $scope.nots.howOftenOptions[0];
+                    $scope.nots.dayOfWeek = $scope.nots.daysOfWeek[2];
+                    $scope.nots.dayOfMonth = $scope.nots.daysOfMonth[0];
+                    $scope.nots.all = !$rootScope.me.settings.notifications.props || !$rootScope.me.settings.notifications.props.length;
+
+                    var cron = $rootScope.me.settings.notifications.cron.split(" ");
+
+                    if (cron[4] == "*") {
+                        $scope.nots.howOften = $scope.nots.howOftenOptions[1]
+                    }
+                    else {
+                        $scope.nots.dayOfWeek = _.find($scope.nots.daysOfWeek, function(x) {
+                            return x.id.toString() == cron[4];
+                        })
                     }
 
                     unbind();
