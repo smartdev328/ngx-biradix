61bd40cc1197dd8e5c838f4f75a4a4ddbc6ce5aa
alex_viderman <Alex1212>
2015-12-12 16:10:16 -0700

    NP-256: Add consumer to process a single user for notifications

--- api/properties/services/dashboardService.js
+++ api/properties/services/dashboardService.js
@@ -4,6 +4,7 @@ var _ = require("lodash")
 var moment = require('moment');
 var PropertyService = require('../services/propertyService')
 var DataPointsService = require('../services/dataPointsService')
+var SurveyHelperService = require('../services/surveyHelperService')
 
 module.exports = {
     getProfile: function(user,options,checkManaged, subjectId, compId, callback) {
@@ -66,7 +67,10 @@ module.exports = {
 
                 async.parallel({
                     comps: function (callbackp) {
-                        PropertyService.getLastSurveyStats(user.settings.hideUnlinked, all.subject, comps, function() {
+                        PropertyService.getLastSurveyStats({
+                            hide: user.settings.hideUnlinked,
+                            injectFloorplans: true
+                        }, all.subject, comps, function() {
                             callbackp(null, comps)
                         })
                     },
@@ -119,6 +123,7 @@ module.exports = {
     },
 
     getDashboard: function(user,id,options,callback) {
+        options.injectFloorplans = options.injectFloorplans === false ? false : true;
         var timer = new Date().getTime();
         PropertyService.search(user, {limit: 1, permission: 'PropertyManage', _id: id
             , select: "_id name address city state zip phone contactEmail contactName website owner management constructionType yearBuilt yearRenovated loc totalUnits survey comps"
@@ -146,44 +151,77 @@ module.exports = {
                     if (err) {
                         return callback(err,null)
                     } else {
-                        async.parallel({
-                            comps: function (callbackp) {
-                                PropertyService.getLastSurveyStats(user.settings.hideUnlinked, property[0], comps, function() {
-                                    callbackp(null, comps)
-                                })
-                            },
-                            points: function(callbackp) {
-                                DataPointsService.getPoints(user.settings.hideUnlinked, property[0], comps,
-                                    options.summary,
-                                    options.bedrooms,
-                                    options.daterange,
-                                    options.offset,
-                                    options.show,
-                                    function(points) {
-                                        callbackp(null, points)
+                        //If we pass in a surveyDate, dont use the last survey date in comps.survey.id
+                        //Instead get the last survey older then the date given
+                        updateCompSurveyIdsByDate(comps,options.surveyDate, function() {
+                            async.parallel({
+                                comps: function (callbackp) {
+                                    PropertyService.getLastSurveyStats({
+                                        hide: user.settings.hideUnlinked,
+                                        injectFloorplans: options.injectFloorplans
+                                    }, property[0], comps, function() {
+                                        callbackp(null, comps)
                                     })
-                            }
-                        }, function(err, all) {
-                            all.comps.forEach(function(c) {
-                                delete c.floorplans;
-                            })
+                                },
+                                points: function(callbackp) {
+                                    if (options.skipPoints) {
+                                        callbackp(null, null)
+                                    }
+                                    else {
+                                        DataPointsService.getPoints(user.settings.hideUnlinked, property[0], comps,
+                                            options.summary,
+                                            options.bedrooms,
+                                            options.daterange,
+                                            options.offset,
+                                            options.show,
+                                            function (points) {
+                                                callbackp(null, points)
+                                            })
+                                    }
+                                }
+                            }, function(err, all) {
+                                all.comps.forEach(function(c) {
+                                    delete c.floorplans;
+                                })
 
-                            //console.log("Dashboard DB for " + id + ": " + (new Date().getTime() - timer) + "ms");
+                                //console.log("Dashboard DB for " + id + ": " + (new Date().getTime() - timer) + "ms");
 
-                            callback (null,{property: property[0], comps: all.comps, points: all.points});
+                                callback (null,{property: property[0], comps: all.comps, points: all.points});
 
-                            for (var s in all) {
-                                all[s] = null;
-                                delete all[s];
-                            }
-                            all = null;
-                        });
+                                for (var s in all) {
+                                    all[s] = null;
+                                    delete all[s];
+                                }
+                                all = null;
+                            });
 
-
+                        })
                     }
                 });
             }
 
         })
+    }
+}
+
+function updateCompSurveyIdsByDate(comps,surveyDate, callback) {
+    if (!surveyDate) {
+        callback();
+    }
+    else {
+        async.each(comps, function(comp, callbackp) {
+            SurveyHelperService.getSurveyBeforeDate(comp._id, surveyDate, function(err, surveys) {
+                if (!surveys || surveys.length == 0) {
+                    delete comp.survey;
+                } else {
+                    comp.survey.id = surveys[0]._id.toString();
+                }
+                callbackp(null);
+            })
+
+        }, function(err, all) {
+            callback();
+        })
+
     }
 }
