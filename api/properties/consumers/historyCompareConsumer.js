17c78c1ce9db1545d604a8dcc50d4d98063430f1
alex_viderman <Alex1212>
2015-12-13 20:47:41 -0700

    NP-256: Add consumer to process a single user for notifications

--- api/properties/consumers/historyCompareConsumer.js
+++ api/properties/consumers/historyCompareConsumer.js
@@ -18,7 +18,7 @@ queues.getHistoryCompareReportQueue().consume(function(data,reply) {
                 var report = [];
 
                 dashboard.comps.forEach(function(c) {
-                    report.push({name: c.name, _id: c._id, ner: c.survey.ner, rent: c.survey.rent, nersqft: c.survey.nersqft, totUnits: c.survey.totUnits, date: c.survey.date, occupancy: c.survey.occupancy, tier: c.survey.tier});
+                    report.push({name: c.name, _id: c._id, sqft: c.survey.sqft, ner: c.survey.ner, rent: c.survey.rent, nersqft: c.survey.nersqft, totUnits: c.survey.totUnits, date: c.survey.date, occupancy: c.survey.occupancy, tier: c.survey.tier});
                 })
 
                 callbackp(null, report);
@@ -39,7 +39,7 @@ queues.getHistoryCompareReportQueue().consume(function(data,reply) {
                 var report = [];
 
                 dashboard.comps.forEach(function(c) {
-                    report.push({name: c.name, _id: c._id, ner: c.survey.ner, rent: c.survey.rent, nersqft: c.survey.nersqft, totUnits: c.survey.totUnits, date: c.survey.date, occupancy: c.survey.occupancy, tier: c.survey.tier});
+                    report.push({name: c.name, _id: c._id, sqft: c.survey.sqft, ner: c.survey.ner, rent: c.survey.rent, nersqft: c.survey.nersqft, totUnits: c.survey.totUnits, date: c.survey.date, occupancy: c.survey.occupancy, tier: c.survey.tier});
                 })
 
                 callbackp(null, report);
@@ -61,7 +61,7 @@ queues.getHistoryCompareReportQueue().consume(function(data,reply) {
                 var report = [];
 
                 dashboard.comps.forEach(function (c) {
-                    report.push({name: c.name, _id: c._id, ner: c.survey.ner, rent: c.survey.rent, nersqft: c.survey.nersqft, totUnits: c.survey.totUnits, date: c.survey.date, occupancy: c.survey.occupancy, tier: c.survey.tier});
+                    report.push({name: c.name, _id: c._id, sqft: c.survey.sqft, ner: c.survey.ner, rent: c.survey.rent, nersqft: c.survey.nersqft, totUnits: c.survey.totUnits, date: c.survey.date, occupancy: c.survey.occupancy, tier: c.survey.tier});
                 })
 
                 callbackp(null, report);
@@ -72,21 +72,59 @@ queues.getHistoryCompareReportQueue().consume(function(data,reply) {
 
         var report = all.current;
 
-        report.forEach(function(p) {
+        var totalrow = {name: 'Comp Averages'};
+
+        report.forEach(function(p,i) {
+            if (i > 0) {
+                totalrow.totUnits = (totalrow.totUnits || 0) + p.totUnits;
+                totalrow.occupancy = (totalrow.occupancy || 0) + (p.occupancy * p.totUnits);
+                totalrow.sqft = (totalrow.sqft || 0) + (p.sqft * p.totUnits);
+                totalrow.rent = (totalrow.rent || 0) + (p.rent * p.totUnits);
+                totalrow.ner = (totalrow.ner || 0) + (p.ner * p.totUnits);
+                totalrow.nersqft = (totalrow.nersqft || 0) + (p.nersqft * p.totUnits);
+            }
+
             var lastweek = _.find(all.lastweek, function(x) {return x._id.toString() == p._id.toString()});
             var lastmonth = _.find(all.lastmonth, function(x) {return x._id.toString() == p._id.toString()});
 
             if (p.nersqft && lastweek && lastweek.nersqft) {
                 p.lastweeknersqft = lastweek.nersqft;
                 p.lastweeknersqftpercent = Math.round((p.nersqft - lastweek.nersqft) / p.nersqft * 100 * 10) / 10;
+
+                if (i > 0) {
+                    totalrow.lastweeknersqft = (totalrow.lastweeknersqft || 0) + (p.lastweeknersqft * p.totUnits);
+                }
             }
 
             if (p.nersqft && lastmonth && lastmonth.nersqft) {
                 p.lastmonthnersqft = lastmonth.nersqft;
                 p.lastmonthnersqftpercent = Math.round((p.nersqft - lastmonth.nersqft) / p.nersqft * 100 * 10) / 10;
+
+                if (i > 0) {
+                    totalrow.lastmonthnersqft = (totalrow.lastmonthnersqft || 0) + (p.lastmonthnersqft * p.totUnits);
+                }
+            }
+        })
+
+        if (totalrow.totUnits && totalrow.totUnits > 0) {
+
+            totalrow.occupancy = Math.round(totalrow.occupancy / totalrow.totUnits * 10) / 10;
+            totalrow.sqft = Math.round(totalrow.sqft / totalrow.totUnits);
+            totalrow.rent = Math.round(totalrow.rent / totalrow.totUnits);
+            totalrow.ner = Math.round(totalrow.ner / totalrow.totUnits * 100) / 100;
+            totalrow.nersqft = Math.round(totalrow.nersqft / totalrow.totUnits * 100) / 100;
+            totalrow.lastweeknersqft = Math.round(totalrow.lastweeknersqft / totalrow.totUnits * 100) / 100;
+            totalrow.lastmonthnersqft = Math.round(totalrow.lastmonthnersqft / totalrow.totUnits * 100) / 100;
+
+            if (totalrow.lastweeknersqft) {
+                totalrow.lastweeknersqftpercent = Math.round((totalrow.nersqft - totalrow.lastweeknersqft ) / totalrow.nersqft * 100 * 10) / 10;
             }
 
-        })
+            if (totalrow.lastmonthnersqft) {
+                totalrow.lastmonthnersqftpercent = Math.round((totalrow.nersqft - totalrow.lastmonthnersqft ) / totalrow.nersqft * 100 * 10) / 10;
+            }
+        }
+        report.push(totalrow);
 
         reply({err: err, report: report});
     })
