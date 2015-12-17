4b087218b5d8089b146f914b043459b72b654c8c
alex_viderman <Alex1212>
2015-12-16 21:30:48 -0700

    NP-266: Property Status Update - Email Text changes NP-270: Style changes for the status update email

--- api/progress/services/progressService.js
+++ api/progress/services/progressService.js
@@ -2,7 +2,7 @@ var RedisService = require('../../utilities/services/redisService')
 
 module.exports = {
     setComplete: function(progressId) {
-        RedisService.set(progressId, progressId, 60 * 5 * 1000);
+        RedisService.set(progressId, progressId, 5);
     },
     isComplete: function(progressId, callback) {
         RedisService.get(progressId, function(err, result) {
