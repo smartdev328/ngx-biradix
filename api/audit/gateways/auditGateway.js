8d5d57e4947e9c5d3eebe51a6e6ba2af49bc8eb9
alex_viderman <Alex1212>
2016-07-13 21:12:32 -0700

    NP-417: Disable ability to undo actions on locked properties

--- api/audit/gateways/auditGateway.js
+++ api/audit/gateways/auditGateway.js
@@ -199,10 +199,25 @@ Routes.post('/', function (req, res) {
                     res.status(200).json({errors: err});
                 }
                 else {
-                    res.status(200).json({errors: null, activity: obj, pager: pager});
-                }
+                    var propertyids = _.pluck(obj,"property.id");
+                    _.remove(propertyids, function(x) {return !x});
+                    propertyids = propertyids.map(function(x) {return x.toString()});
+                    propertyids = _.uniq(propertyids);
 
-                obj = null;
+                    obj = JSON.parse(JSON.stringify(obj));
+                    PropertyService.search(req.user, {limit: 1000, permission: ['PropertyManage'], ids: propertyids
+                        , select: "_id"
+                    }, function(err, properties) {
+                        obj.forEach(function(o) {
+                            o.canUndo = true;
+                            if (o.property && o.property.orgid && !_.find(properties, function(x) {return x._id.toString() == o.property.id.toString()})) {
+                                o.canUndo = false;
+                            }
+                        })
+                        res.status(200).json({errors: null, activity: obj, pager: pager});
+                        obj = null;
+                    })
+                }
             });
     })
 });
