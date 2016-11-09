9c845d062a79a222490031a19303622f1cf21550
alex_viderman <Alex1212>
2016-11-08 22:54:13 -0700

    try catch consumers

--- api/properties/consumers/dashboardConsumer.js
+++ api/properties/consumers/dashboardConsumer.js
@@ -3,22 +3,34 @@ var DashboardService = require("../services/dashboardService")
 
 queues.getDashboardQueue().consume(function(data,reply) {
     console.log(data.id + " dashboard started");
-    DashboardService.getDashboard(data.user,data.id, data.options, function(err, dashboard) {
-        //console.log(data.id + " dashboard ended");
-        reply({err: err, dashboard: dashboard});
-        dashboard = null;
-    })
+    try {
+        DashboardService.getDashboard(data.user, data.id, data.options, function (err, dashboard) {
+            //console.log(data.id + " dashboard ended");
+            reply({err: err, dashboard: dashboard});
+            dashboard = null;
+        })
+    }
+    catch (ex) {
+        reply({err: ex, dashboard: null});
+        throw ex;
+    }
 });
 
 queues.attachQListeners(queues.getDashboardQueue(), "Dashboard");
 
 queues.getProfileQueue().consume(function(data,reply) {
     //console.log(data.compId + " profile started: " + (new Date()));
-    DashboardService.getProfile(data.user,data.options, data.checkManaged, data.subjectId, data.compId, function(err, profile) {
-        //console.log(data.compId + " profile ended: " + (new Date()));
-        reply({err: err, profile: profile});
-        profile = null;
-    })
+    try {
+        DashboardService.getProfile(data.user, data.options, data.checkManaged, data.subjectId, data.compId, function (err, profile) {
+            //console.log(data.compId + " profile ended: " + (new Date()));
+            reply({err: err, profile: profile});
+            profile = null;
+        })
+    }
+    catch (ex) {
+        reply({err: ex, profile: null});
+        throw ex;
+    }
 });
 
 queues.attachQListeners(queues.getDashboardQueue(), "Profile");
--- api/properties/consumers/pdfConsumer.js
+++ api/properties/consumers/pdfConsumer.js
@@ -10,95 +10,106 @@ var ProgressService = require('../../progress/services/progressService')
 
 queues.getPdfProfileQueue().consume(function(data,reply) {
     console.log(data.id + " pdf started");
-    PropertyService.search(data.user, {_id: data.id}, function(err, properties) {
-        UserService.getFullUser(data.user, function(full) {
-            var p = properties[0];
-            var fileName = p.name.replace(/ /g, "_");
+    try {
+        PropertyService.search(data.user, {_id: data.id}, function (err, properties) {
+            UserService.getFullUser(data.user, function (full) {
+                var p = properties[0];
+                var fileName = p.name.replace(/ /g, "_");
 
-            if (data.full) {
-                fileName += '_and_Comps';
-            }
-
-            //console.log(data.timezone, 'Naked: ',moment().format());
-            //console.log('Utc: ',moment().utc().format());
-            //console.log('Utc Offset: ',moment().utc().utcOffset(data.timezone).format());
-            //console.log('Add: ',moment().utc().add(data.timezone,"minute").format());
-
-            fileName += "_" + moment().utc().add(data.timezone,"minute").format("MM_DD_YYYY");
-
-            fileName += ".pdf";
-
-            var options = pdfService.getDefaultOptions()
-
-            var render = phantom(options);
-
-            var url = data.url + "/#/" + (data.full ? "full" : "profile") + "/" + p._id;
-
-            var cookies = [
-                pdfService.getCookie(data.hostname,"token", full.token),
-                pdfService.getCookie(data.hostname,"Graphs", data.Graphs),
-                pdfService.getCookie(data.hostname,"Summary", data.Summary),
-                pdfService.getCookie(data.hostname,"Scale", data.Scale),
-                pdfService.getCookie(data.hostname,"selectedStartDate", data.selectedStartDate),
-                pdfService.getCookie(data.hostname,"selectedEndDate", data.selectedEndDate),
-                pdfService.getCookie(data.hostname,"selectedRange", data.selectedRange),
-                pdfService.getCookie(data.hostname,"fp.o", data.orderBy),
-                pdfService.getCookie(data.hostname,"fp.s", data.show),
-                pdfService.getCookie(data.hostname,"cmp.o", data.orderByComp),
-                pdfService.getCookie(data.hostname,"cmp.s", data.showComp),
-                pdfService.getCookie(data.hostname,"pr.s", data.showProfile),
-
-            ];
-
-
-            var description = p.name;
-
-            if (data.full) {
-                description += ' (with Comps)';
-            }
-
-            description += " - " + data.selectedRange;
-
-            AuditService.create({type: 'pdf_profile', operator: data.user, property: p, description: description, context: data.context})
-
-
-            options.cookies = cookies;
-
-            var MemoryStream = require('memory-stream');
-
-            var ws = new MemoryStream();
-
-
-            ws.on('finish', function() {
-                var newBuffer = Buffer.concat(ws.buffer);
-                if (data.progressId) {
-                    ProgressService.setComplete(data.progressId)
+                if (data.full) {
+                    fileName += '_and_Comps';
                 }
 
-                var JSONB = require('json-buffer')
+                //console.log(data.timezone, 'Naked: ',moment().format());
+                //console.log('Utc: ',moment().utc().format());
+                //console.log('Utc Offset: ',moment().utc().utcOffset(data.timezone).format());
+                //console.log('Add: ',moment().utc().add(data.timezone,"minute").format());
 
-                console.log(data.id + " pdf ended");
-                reply({stream: JSONB.stringify(newBuffer), filename: fileName});
-                full = null;
-                cookies = null;
-                r = null;
-                render = null;
-                options = null;
-                properties = null;
-                newBuffer = null;
+                fileName += "_" + moment().utc().add(data.timezone, "minute").format("MM_DD_YYYY");
 
-                settings.PDF_HIT_COUNT ++;;
+                fileName += ".pdf";
+
+                var options = pdfService.getDefaultOptions()
+
+                var render = phantom(options);
+
+                var url = data.url + "/#/" + (data.full ? "full" : "profile") + "/" + p._id;
+
+                var cookies = [
+                    pdfService.getCookie(data.hostname, "token", full.token),
+                    pdfService.getCookie(data.hostname, "Graphs", data.Graphs),
+                    pdfService.getCookie(data.hostname, "Summary", data.Summary),
+                    pdfService.getCookie(data.hostname, "Scale", data.Scale),
+                    pdfService.getCookie(data.hostname, "selectedStartDate", data.selectedStartDate),
+                    pdfService.getCookie(data.hostname, "selectedEndDate", data.selectedEndDate),
+                    pdfService.getCookie(data.hostname, "selectedRange", data.selectedRange),
+                    pdfService.getCookie(data.hostname, "fp.o", data.orderBy),
+                    pdfService.getCookie(data.hostname, "fp.s", data.show),
+                    pdfService.getCookie(data.hostname, "cmp.o", data.orderByComp),
+                    pdfService.getCookie(data.hostname, "cmp.s", data.showComp),
+                    pdfService.getCookie(data.hostname, "pr.s", data.showProfile),
+
+                ];
+
+
+                var description = p.name;
+
+                if (data.full) {
+                    description += ' (with Comps)';
+                }
+
+                description += " - " + data.selectedRange;
+
+                AuditService.create({
+                    type: 'pdf_profile',
+                    operator: data.user,
+                    property: p,
+                    description: description,
+                    context: data.context
+                })
+
+
+                options.cookies = cookies;
+
+                var MemoryStream = require('memory-stream');
+
+                var ws = new MemoryStream();
+
+
+                ws.on('finish', function () {
+                    var newBuffer = Buffer.concat(ws.buffer);
+                    if (data.progressId) {
+                        ProgressService.setComplete(data.progressId)
+                    }
+
+                    var JSONB = require('json-buffer')
+
+                    console.log(data.id + " pdf ended");
+                    reply({stream: JSONB.stringify(newBuffer), filename: fileName});
+                    full = null;
+                    cookies = null;
+                    r = null;
+                    render = null;
+                    options = null;
+                    properties = null;
+                    newBuffer = null;
+
+                    settings.PDF_HIT_COUNT++;
+                    ;
+                });
+
+                var r = render(url, options).pipe(ws);
+
+
             });
 
-            var r = render(url, options).pipe(ws);
 
-
-        });
-
-
-
-
-    })    
+        })
+    }
+    catch (ex) {
+        reply({err: ex});
+        throw ex;
+    }
 
 });
 
@@ -106,65 +117,71 @@ queues.getPdfProfileQueue().consume(function(data,reply) {
 queues.getPdfReportingQueue().consume(function(data,reply) {
     console.log(data.id + " reporting pdf started");
 
-    PropertyService.search(data.user, {_id: data.id}, function(err, properties) {
-        UserService.getFullUser(data.user, function (full) {
-            var p = properties[0];
-            var fileName = p.name.replace(/ /g, "_");
+    try {
+        PropertyService.search(data.user, {_id: data.id}, function (err, properties) {
+            UserService.getFullUser(data.user, function (full) {
+                var p = properties[0];
+                var fileName = p.name.replace(/ /g, "_");
 
-            if (data.type == "multiple") {
-                fileName = "Portfolio"
-            }
-
-            fileName += "_Report_" + moment().utc().add(data.timezone,"minute").format("MM_DD_YYYY");
-
-            fileName += ".pdf";
-
-            var options = pdfService.getDefaultOptions();
-
-            var render = phantom(options);
-
-            var url = data.url + "/#/reporting";
-
-            var cookies = [
-                pdfService.getCookie(data.hostname,"token", full.token),
-                pdfService.getCookie(data.hostname,"compIds", data.compIds),
-                pdfService.getCookie(data.hostname,"reportIds", data.reportIds),
-                pdfService.getCookie(data.hostname,"subjectId", data.id),
-                pdfService.getCookie(data.hostname,"type", data.type),
-                pdfService.getCookie(data.hostname,"propertyIds", data.propertyIds),
-            ];
-
-            options.cookies = cookies;
-
-            var MemoryStream = require('memory-stream');
-
-            var ws = new MemoryStream();
-
-
-            ws.on('finish', function() {
-                var newBuffer = Buffer.concat(ws.buffer);
-                if (data.progressId) {
-                    ProgressService.setComplete(data.progressId)
+                if (data.type == "multiple") {
+                    fileName = "Portfolio"
                 }
 
-                var JSONB = require('json-buffer')
+                fileName += "_Report_" + moment().utc().add(data.timezone, "minute").format("MM_DD_YYYY");
 
-                console.log(data.id + " pdf reporting ended");
-                reply({stream: JSONB.stringify(newBuffer), filename: fileName});
-                full = null;
-                cookies = null;
-                r = null;
-                render = null;
-                options = null;
-                properties = null;
-                newBuffer = null;
-                settings.PDF_HIT_COUNT ++;;
+                fileName += ".pdf";
+
+                var options = pdfService.getDefaultOptions();
+
+                var render = phantom(options);
+
+                var url = data.url + "/#/reporting";
+
+                var cookies = [
+                    pdfService.getCookie(data.hostname, "token", full.token),
+                    pdfService.getCookie(data.hostname, "compIds", data.compIds),
+                    pdfService.getCookie(data.hostname, "reportIds", data.reportIds),
+                    pdfService.getCookie(data.hostname, "subjectId", data.id),
+                    pdfService.getCookie(data.hostname, "type", data.type),
+                    pdfService.getCookie(data.hostname, "propertyIds", data.propertyIds),
+                ];
+
+                options.cookies = cookies;
+
+                var MemoryStream = require('memory-stream');
+
+                var ws = new MemoryStream();
+
+
+                ws.on('finish', function () {
+                    var newBuffer = Buffer.concat(ws.buffer);
+                    if (data.progressId) {
+                        ProgressService.setComplete(data.progressId)
+                    }
+
+                    var JSONB = require('json-buffer')
+
+                    console.log(data.id + " pdf reporting ended");
+                    reply({stream: JSONB.stringify(newBuffer), filename: fileName});
+                    full = null;
+                    cookies = null;
+                    r = null;
+                    render = null;
+                    options = null;
+                    properties = null;
+                    newBuffer = null;
+                    settings.PDF_HIT_COUNT++;
+                    ;
+                });
+
+                var r = render(url, options).pipe(ws);
             });
-
-            var r = render(url, options).pipe(ws);
         });
-    });
-
+    }
+    catch (ex) {
+        reply({err: ex});
+        throw ex;
+    }
 
 });
 
