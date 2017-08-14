8eb2b1547ed1d842ff2e7a3a4c41a63c03816b52
alex_viderman <Alex1212>
2017-08-13 19:57:32 -0700

    more ajax

--- site/components/filterlist/moduleajax.js
+++ site/components/filterlist/moduleajax.js
@@ -58,6 +58,75 @@
 
                 $scope.performSearch();
 
+                $scope.resetChecked = function() {
+                    $scope.items.forEach(function(item) {
+                        item.checked = false;
+                    })
+
+                    $scope.model.forEach(function(item) {
+                        item.checked = false;
+                    })
+                }
+
+                $scope.resetFocused = function() {
+                    $scope.items.forEach(function(item) {
+                        item.focused = false;
+                    })
+
+                    $scope.model.forEach(function(item) {
+                        item.focused = false;
+                    })
+                }
+
+                $scope.moveChecked = function (state) {
+                    $scope.resetFocused();
+
+                    var ar;
+
+                    if (!state) {
+                        ar = $scope.items
+                    } else {
+                        ar = $scope.model;
+                    }
+
+
+                    var rem = [];
+                    ar.forEach(function (item) {
+                        //console.log(item,item.checked,state)
+                        if (item.checked) {
+                            item.focused = true;
+                            rem.push(item.id);
+
+                        }
+                    });
+
+                    if (!state) {
+                        $scope.model = $scope.model.concat(_.filter(ar, function(x) {return rem.indexOf(x.id) > -1}));
+                    } else {
+                        $scope.items = $scope.items.concat(_.filter(ar, function(x) {return rem.indexOf(x.id) > -1}));
+                    }
+                    _.remove(ar, function(x) {return rem.indexOf(x.id) > -1})
+
+
+                    $scope.resetChecked();
+                    $scope.hideSelected();
+
+                }
+
+                $scope.all = function (state) {
+                    var ar;
+
+                    if (state) {
+                        ar = $scope.items
+                    } else {
+                        ar = $scope.model;
+                    }
+
+                    ar.forEach(function(item) {
+                        item.checked = true;
+                    })
+                    $scope.moveChecked(!state);
+                }
 
             },
             templateUrl: '/components/filterlist/filterlistajax.html?bust=' + version
