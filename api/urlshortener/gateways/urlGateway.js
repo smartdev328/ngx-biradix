d39bf461f629b34620288b4e15262f3cdb4125cb
alex_viderman <Alex1212>
2017-09-16 15:14:03 -0700

    Fix load report

--- api/urlshortener/gateways/urlGateway.js
+++ api/urlshortener/gateways/urlGateway.js
@@ -9,10 +9,11 @@ Routes.post('/', function (req, res) {
     var response = "";
 
     if (req.body.url) {
+        console.log('Date 1: ', new Date());
         var key = md5(req.body.url);
-
+        console.log('Date 2: ', new Date());
         redisService.set(req.body.url,req.body.url,30);
-
+        console.log('Date 3: ', new Date());
         return res.status(200).json({key: key});
     }
     else {
