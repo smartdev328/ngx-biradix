15080cdba8a00a3a493d19bec3fa634187d61568
alex_viderman <Alex1212>
2017-10-09 15:11:05 -0700

    NP-792: Re: Connect multiple graphs with hover

--- site/components/reports/trendsTimeseries.js
+++ site/components/reports/trendsTimeseries.js
@@ -203,9 +203,9 @@ define([
                                 },
                                 title: {
                                     text: $scope.options.title,
-                                    align: 'center',
+                                    align: 'left',
                                     margin: 0,
-                                    // x: 40
+                                    x: 55
                                 },
                                 xAxis: {
                                     crosshair: false,
@@ -266,7 +266,7 @@ define([
                                 legend: {
                                     layout: isMobile ? 'horizontal' :  'vertical',
                                     align: isMobile ? 'left' :'right',
-                                    verticalAlign: isMobile ? 'bottom' : 'top',
+                                    verticalAlign: isMobile ? 'bottom' : 'middle',
                                     borderWidth: 0,
                                 },
                                 series: data
