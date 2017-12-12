eccee47e92286413da12e95918af7b4234193c51
alex_viderman <Alex1212>
2017-12-11 17:01:59 -0700

    thumbnails

--- site/components/gallery/module.js
+++ site/components/gallery/module.js
@@ -5,8 +5,44 @@ angular.module('biradix.global').directive('gallery', function () {
             options: '=',
             images: '=',
         },
-        link: function() {
+        link: function($scope) {
+            $scope.attachment = false;
+            $scope.canClick = true;
+
+            $.fn.attachDragger = function(){
+                var lastPosition, position, difference;
+                $( $(this).selector ).on("mousedown mouseup mousemove",function(e){
+                    if( e.type == "mousedown" ) {
+                        $scope.attachment = true;
+                        lastPosition = [e.clientX, e.clientY];
+                    }
+                    if( e.type == "mouseup" ) {
+                        $scope.attachment = false;
+                        window.setTimeout(function() {
+                            $scope.canClick = true;
+                        }, 200)
+
+                    }
+                    if( e.type == "mousemove" && $scope.attachment == true ){
+                        $scope.canClick = false;
+                        position = [e.clientX, e.clientY];
+                        difference = [ (position[0]-lastPosition[0]), (position[1]-lastPosition[1]) ];
+                        $(this).scrollLeft( $(this).scrollLeft() - difference[0] );
+                        $(this).scrollTop( $(this).scrollTop() - difference[1] );
+                        lastPosition = [e.clientX, e.clientY];
+                    }
+                });
+                $(window).on("mouseup", function(){
+                    $scope.attachment = false;
+                    window.setTimeout(function() {
+                        $scope.canClick = true;
+                    }, 200)
+                });
+            }
+
             $(document.body).append($('.gallery-overlay').detach());
+
+            $(".gallery-overlay .thumbs").attachDragger();
         },
         controller: function ($scope, $element,toastr) {
 
@@ -28,9 +64,12 @@ angular.module('biradix.global').directive('gallery', function () {
             }
 
             $scope.select = function(i) {
+                if ($scope.canClick == false) {
+                    return;
+                }
                 $scope.index = i;
 
-                var scroll = 105 * (i - 1.5);
+                var scroll = 155 * (i - 1.5);
 
                 if (scroll < 0) {
                     scroll = 0;
