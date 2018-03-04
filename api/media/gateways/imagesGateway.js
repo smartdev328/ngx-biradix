d897371b43c7bbf233e82cca9971f0fb902c9102
alex_viderman <Alex1212>
2018-03-03 20:27:20 -0700

    s3 logging

--- api/media/gateways/imagesGateway.js
+++ api/media/gateways/imagesGateway.js
@@ -1,41 +1,39 @@
-'use strict';
-var express = require('express');
-var Routes = express.Router();
-var settings = require("../../../config/settings")
-var AWS = require('aws-sdk');
-var uuid = require("node-uuid");
+"use strict";
+const express = require("express");
+const Routes = express.Router();
+const settings = require("../../../config/settings");
+const AWS = require("aws-sdk");
+const uuid = require("node-uuid");
 
-Routes.post('/', function (req, res) {
-    var s3Bucket = new AWS.S3( { params: {Bucket: settings.AWS_S3_BUCKET_IMAGES} } );
-    var buf = new Buffer(req.body.image.replace(/^data:image\/\w+;base64,/, ""),'base64')
+Routes.post("/", function(req, res) {
+    let s3Bucket = new AWS.S3({params: {Bucket: settings.AWS_S3_BUCKET_IMAGES}});
+    let buf = new Buffer(req.body.image.replace(/^data:image\/\w+;base64,/, ""), "base64");
 
-    var file = uuid.v1() + ".jpg";
-    var path = "/";
-    var url = "https://" + settings.AWS_S3_CDN_IMAGES + path + file;
+    let file = uuid.v1() + ".jpg";
+    let path = "/";
+    let url = "https://" + settings.AWS_S3_CDN_IMAGES + path + file;
 
-    var data = {
-        ACL: 'public-read',
+    let data = {
+        ACL: "public-read",
         Key: file,
         Body: buf,
-        ContentEncoding: 'base64',
-        ContentType: 'image/jpeg'
+        ContentEncoding: "base64",
+        ContentType: "image/jpeg",
     };
-    s3Bucket.putObject(data, function(err, data){
+    s3Bucket.putObject(data, function(err, data) {
         if (err) {
-            console.log(err);
+            console.log("S3 Error: ", err);
             throw new Error(err);
         } else {
             return res.status(200).json({
                 image: {
                     name: req.body.name,
-                    url : url,
+                    url: url,
                     width: req.body.width,
-                    height: req.body.height
+                    height: req.body.height,
                 }});
         }
     });
-
-
 });
 
 module.exports = Routes;
--- api/media/services/s3Service.js
+++ api/media/services/s3Service.js
@@ -1,27 +1,29 @@
-var settings = require("../../../config/settings")
-var AWS = require('aws-sdk');
-var uuid = require("node-uuid");
-var _ = require("lodash")
+const settings = require("../../../config/settings");
+const AWS = require("aws-sdk");
+const uuid = require("node-uuid");
+const _ = require("lodash");
 
 module.exports = {
-    copyImage : function(source, callback) {
-        var s3Bucket = new AWS.S3( { params: {Bucket: settings.AWS_S3_BUCKET_IMAGES} } );
+    copyImage: function(source, callback) {
+        let s3Bucket = new AWS.S3({params: {Bucket: settings.AWS_S3_BUCKET_IMAGES}});
 
-        var file = uuid.v1() + ".jpg";
-        var path = "/";
-        var url = "https://" + settings.AWS_S3_CDN_IMAGES + path + file;
+        let file = uuid.v1() + ".jpg";
+        let path = "/";
+        let url = "https://" + settings.AWS_S3_CDN_IMAGES + path + file;
 
-        var params = {
-            ACL: 'public-read',
+        let params = {
+            ACL: "public-read",
             CopySource: source.url.replace("https://" + settings.AWS_S3_CDN_IMAGES, settings.AWS_S3_BUCKET_IMAGES),
-            Key: file
+            Key: file,
         };
 
-        s3Bucket.copyObject(params, function(copyErr, copyData){
-            var newMedia = _.cloneDeep(source);
+        s3Bucket.copyObject(params, function(copyErr, copyData) {
+            if (copyErr) {
+                console.log("S3 Error: ", copyErr);
+            }
+            let newMedia = _.cloneDeep(source);
             newMedia.url = url;
             callback(copyErr, copyErr ? null : newMedia);
         });
-
-    }
-}
+    },
+};
