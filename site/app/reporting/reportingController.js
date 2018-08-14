99a3375a7600281031712e05b22545060e9fa9c3
Alex <alex@viderman.com>
2018-08-14 14:32:58 -0700

    Updates

--- site/app/reporting/reportingController.js
+++ site/app/reporting/reportingController.js
@@ -27,9 +27,9 @@ define([
 
         $scope.meLoaded = false;
         var me = $rootScope.$watch("me", function(x) {
-            console.log($rootScope.me);
+            console.log($rootScope.me, x);
             if ($rootScope.me) {
-                console.log($rootScope.me, "Inside");
+                console.log($rootScope.me, x, "Inside");
                 $scope.reportItems.push({id: "community_amenities", name: "Community Amenities", selected:false, group: "Individual Reports", type:"single", tooltip: "<b>Community Amenities Report</b><Br><i>Report highlights all Community Amenities for the subject property and each comp property.</i>"});
                 $scope.reportItems.push({id: "concession", name: "Concessions", selected:$stateParams.property == "2", group: "Individual Reports", type:"single", tooltip: "<b>Concessions Report</b><Br><i>Report compares concessions over a customizable period of time for the subject property and comps. It includes Market Rent, One-Time Concessions, Recurring Concessions, Total Concessions, NER, and NER vs. Weighted Avg. Comp NER</i>"});
                 $scope.reportItems.push({id: "fees_deposits", name: "Fees & Deposits", selected:false, group: "Individual Reports", type:"single", tooltip: "<b>Fees and Deposits Report</b><Br><i>Report includes different fees and deposits for each property including: Administration Fee, Application Fee, Lease Terms, Pet Deposit (Refundable & Non-Refundable), Security Deposit, Pet Rent, and Short Term Rental Premium</i>"});
