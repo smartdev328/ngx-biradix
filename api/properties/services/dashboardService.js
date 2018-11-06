9fd6c63ccd7d6e6999bc9e77a7a6c03d3f76165d
Alex <alex@viderman.com>
2018-11-05 20:17:13 -0700

    NP-1129: Ability to run historical Market Survey Reports

--- api/properties/services/dashboardService.js
+++ api/properties/services/dashboardService.js
@@ -151,6 +151,8 @@ module.exports = {
                         all.comp.p.strRangeStart = moment(options.surveyDateStart).utcOffset(options.offset).format("MM/DD/YYYY");
                         all.comp.p.strRangeEnd = moment(options.surveyDateEnd).utcOffset(options.offset).format("MM/DD/YYYY");
                     }
+
+                    console.log(moment(options.surveyDateEnd).format(), options.offset);
                     // console.log("Profile done: " + (new Date().getTime() - timer) / 1000 + "s");
                     callback(null, {property: all.comp.p, comps: all2.comps, lookups: all.comp.l, points: all2.points, canManage: all.modify, owner: all.owner, canSurvey: canSurvey})
 
