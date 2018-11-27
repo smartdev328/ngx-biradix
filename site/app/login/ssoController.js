29423c285e938d09a831c1dc0b8c3bf1aea92576
Alex <alex@viderman.com>
2018-11-26 20:22:20 -0700

    add confluence code, update logic

--- site/app/login/ssoController.js
+++ site/app/login/ssoController.js
@@ -4,34 +4,41 @@ define([
     "../../services/authService"
 ], function (app) {
 
-    app.controller("ssoController", 
-        ["$scope","$rootScope", "$location","toastr", "$authService","$window","$stateParams",
+    app.controller("ssoController", ["$scope","$rootScope", "$location","toastr", "$authService","$window","$stateParams",
             function ($scope,$rootScope,$location,toastr, $authService,$window,$stateParams) {
 
-            if (maintenance === true && $location.path().indexOf("maintenance") == -1) {
-                return $location.path("/maintenance");
+    if (maintenance === true && $location.path().indexOf("maintenance") == -1) {
+        return $location.path("/maintenance");
+    }
+
+    $scope.redirect = function(host) {
+        var domain = "https://" + host;
+
+        if (host === location.hostname) {
+            $location.path("/login").search("r", $stateParams.r);
+        } else {
+            location.href = domain + "/#login?r=" + encodeURIComponent($stateParams.r);
+        }
+    }
+
+    if ($rootScope.loggedIn) {
+        return $scope.redirect($rootScope.me.orgs[0].subdomain + ".biradix.com");
+    }
+    $scope.submit = function() {
+        $scope.localLoading = true;
+
+        $authService.getDomain($scope.email).then(function (domainInfo) {
+            if (domainInfo && domainInfo.data && domainInfo.data.domain) {
+                $scope.redirect(domainInfo.data.domain);
+            } else {
+                toastr.error("Unable to locate your email address.");
+                $scope.localLoading = false;
             }
-                $scope.submit = function() {
-                    $scope.localLoading = true;
-
-                    $authService.getDomain($scope.email).then(function (domainInfo) {
-                            if (domainInfo && domainInfo.data && domainInfo.data.domain) {
-                                var domain = "https://" + domainInfo.data.domain;
-
-                                if (domainInfo.data.domain === location.hostname) {
-                                    $location.path("/login").search("r", $stateParams.r);
-                                } else {
-                                    location.href = domain + "/#login?r=" + encodeURIComponent($stateParams.r);
-                                }
-                            } else {
-                                toastr.error("Unable to locate your email address.");
-                                $scope.localLoading = false;
-                            }
-                        },
-                        function(errors) {
-                            toastr.error('Unable to access the system as this time. Please contact an administrator');
-                            $scope.localLoading = false;
-                        });
-                }
-            }]);
+        },
+            function(errors) {
+                toastr.error('Unable to access the system as this time. Please contact an administrator');
+                $scope.localLoading = false;
+            });
+        }
+    }]);
 });
