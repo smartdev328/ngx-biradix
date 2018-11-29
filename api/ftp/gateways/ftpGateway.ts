deff9357b2c10ca83a8ccadc523d62e2256ae4ab
Alex <alex@viderman.com>
2018-11-28 22:24:03 -0700

    parse ftp round 2

--- api/ftp/gateways/ftpGateway.ts
+++ api/ftp/gateways/ftpGateway.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import {parseDates, parseProperties} from "../services/ftpParsingService";
+import {parseDates, parseFloorplans, parseProperties} from "../services/ftpParsingService";
 import {connect} from "../services/ftpService";
 
 const routes = express.Router();
@@ -23,11 +23,106 @@ routes.get("/date/:date", async (req, res) => {
     let properties = await parseProperties("/pbbell", req.params.date);
 
     properties = properties.map((property) => {
-        return `<li>${property[2]} <i>(${property[3]})</i> [<b>${property[0]}</b> - <u>${property[1]}</u>]</li>`;
+        return `<li><A href="/ftp/date/${req.params.date}/${property.yardiId}">${property.name}</A> [<b>${property.yardiId}</b>]<Br><div style="margin-left:23px"><i>${property.address} - ${property.city} ${property.state}, ${property.zip}</i></div></li>`;
     });
 
     html += properties.join("\r\n");
 
+    return res.status(200).send(html);
+});
+
+routes.get("/date/:date/:yardiId", async (req, res) => {
+    let html = `<h1>/pbbell/${req.params.date}/${req.params.yardiId}</h1>`;
+    html += `<A href="/ftp/date/${req.params.date}">&lt;- Back</A><Br><Br>`;
+    await connect();
+    const properties = await parseProperties("/pbbell", req.params.date);
+    const property = properties.find((p) => {
+        return p.yardiId === req.params.yardiId;
+    })
+    html += `
+        <table border="1">
+            <tr>
+                <th colspan="100%">
+                   Property Info 
+                </th>
+            </tr>
+            <Tr>
+                <td>
+                    <B>Name:</B>
+                </td>
+                <td>
+                    ${property.name}
+                </td>
+            </Tr>
+            <Tr>
+                <td valign="top">
+                    <B>Address</B>
+                </td>
+                <td>
+                    ${property.address}<Br>
+                    ${property.city}, ${property.state} ${property.zip}
+                </td>
+            </Tr>     
+            <Tr>
+                <td>
+                    <B>Yardi Id:</B>
+                </td>
+                <td>
+                    ${property.yardiId}
+                </td>
+            </Tr> 
+        </table>
+    `;
+
+    let floorplans = await parseFloorplans("/pbbell", req.params.date);
+    floorplans = floorplans.filter((p) => {
+        return p.propertyYardiId && p.propertyYardiId.toString() === req.params.yardiId.toString();
+    })
+
+    html += `
+        <br>
+        <table border="1">
+            <tr>
+                <th colspan="100%">
+                   Floor Plans 
+                </th>
+            </tr>
+            <tr>
+                <th>
+                   Type 
+                </th>
+                <th>
+                   Description 
+                </th>
+                <th>
+                   Sqft 
+                </th>
+                <th>
+                   YardiId 
+                </th>
+            </tr>
+    `;
+
+    floorplans.forEach((fp) => {
+       html += `
+             <tr>
+                <td>
+                   ${fp.bedrooms}x${fp.bathrooms} 
+                </td>
+                <td>
+                   ${fp.description} 
+                </td>
+                <td>
+                   ${fp.sqft} 
+                </td>
+                <td>
+                   ${fp.yardiId} 
+                </td>
+            </tr>
+       `;
+    });
+
+    html += `</table>`;
     return res.status(200).send(html);
 });
 
--- api/ftp/services/ftpParsingService.ts
+++ api/ftp/services/ftpParsingService.ts
@@ -1,6 +1,38 @@
 import * as _ from "lodash";
 import {connect, csvParse, downloadFile, sftp} from "./ftpService";
 
+export async function parseFloorplans(folder: string, date: string) {
+    let data = await sftp.list(folder);
+
+    data = data.map((row) => row.name);
+    let propertiesFile = "";
+
+    data.forEach((fileName) => {
+        if (fileName.indexOf("UnitType_" + date) > -1) {
+
+            propertiesFile = fileName;
+        }
+    });
+
+    const body = await downloadFile(folder + "/" + propertiesFile);
+    const unitTypes = await csvParse(body);
+    const unitTypesObjects = [];
+
+    unitTypes.forEach((row) => {
+        unitTypesObjects.push({
+            yardiId: row[0],
+            propertyYardiId: row[2],
+            description: row[3],
+            sqft: parseInt(row[6], 10),
+            bedrooms: row[7],
+            bathrooms: parseInt(row[8], 10),
+
+        });
+    });
+
+    return unitTypesObjects;
+}
+
 export async function parseProperties(folder: string, date: string) {
     let data = await sftp.list(folder);
 
@@ -16,8 +48,20 @@ export async function parseProperties(folder: string, date: string) {
 
     const body = await downloadFile(folder + "/" + propertiesFile);
     const properties = await csvParse(body);
+    const propertiesObjects = [];
 
-    return properties;
+    properties.forEach((row) => {
+       propertiesObjects.push({
+           name: row[2],
+           address: row[3],
+           city: row[5],
+           state: row[6],
+           zip: row[7],
+           yardiId: row[0],
+       });
+    });
+
+    return propertiesObjects;
 }
 
 export async function parseDates(folder: string): Promise<string[]> {
