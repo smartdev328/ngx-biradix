a546958e3d7b5e4415be273083a4f7158f10a2f8
Alex <alex@viderman.com>
2018-12-22 15:01:47 -0700

    Full story integrated with Raygun

--- site/app/main.js
+++ site/app/main.js
@@ -9,6 +9,11 @@ requirejs.config({
 Raygun.init(raygun_key);
 Raygun.setVersion(version);
 
+Raygun.onBeforeSend(function(payload) {
+    payload.Details.UserCustomData.fullStoryUrl = FS.getCurrentSessionURL();
+    return payload;
+});
+
 global_error = function(err,context) {
     if (err) {
         Raygun.send(err);
