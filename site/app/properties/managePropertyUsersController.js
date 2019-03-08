d463144209c5bf6f54d573631776eb307c7d4adc
Alex <alex@viderman.com>
2019-03-07 22:30:30 -0700

    fix search on edit user popup

--- site/app/properties/managePropertyUsersController.js
+++ site/app/properties/managePropertyUsersController.js
@@ -49,7 +49,7 @@ define([
 
                     $userService.search({active: true, orgids: [property.orgid, property.orgid_owner], roleTypes:['RM','BM','PO'], ids: users}).then(function (response) {
                             response.data.users.forEach(function(u) {
-                                $scope.users.push({id: u._id, name: u.name, group: u.roles[0].org.name + " (" + (a.roles[0].org._id.toString() === property.orgid.toString() ? "Management" : "Owner") + ")"});
+                                $scope.users.push({id: u._id, name: u.name, group: u.roles[0].org.name + " (" + (u.roles[0].org._id.toString() === property.orgid.toString() ? "Management" : "Owner") + ")"});
                             });
 
                             $scope.loading = false;
