38ac2bedbcffd58d0a731a6c02f18454fa069325
Alex <alex@viderman.com>
2019-03-24 14:16:06 -0700

    render properties to pick from

--- site/app/properties/pmsSetupController.js
+++ site/app/properties/pmsSetupController.js
@@ -8,6 +8,10 @@ define([
             ga("set", "page", "/pmsSetup");
             ga("send", "pageview");
 
+            $scope.pms = {
+                selectedProperty: null,
+                config: null,
+            };
             $scope.cancel = function() {
                 $uibModalInstance.dismiss("cancel");
             };
@@ -20,7 +24,8 @@ define([
                     ids: [property._id],
                     select: "_id floorplans orgid pms name",
                 }).then(function(response) {
-                    $scope.property = response.data.properties[0];
+                    $scope.property = response.data.properties[0]
+                    $scope.pms.config = property.pms;
                     // TODO: When this becomes client facing, we cannot return all integration client side
                     $importService.read().then(function(response) {
                         $scope.imports = _.filter(response.data, function(i) {
@@ -30,7 +35,12 @@ define([
                         if ($scope.imports.length === 1) {
                             $importIntegrationService.getLatestProperties($scope.imports[0].id).then(function(response) {
                                 $scope.properties = response.data;
-                                console.log($scope.properties);
+
+                                if (!$scope.pms.config) {
+                                    $scope.pms.selectedProperty = _.find($scope.properties, function(p) {
+                                       return p.name === $scope.property.name;
+                                    });
+                                }
                                 $scope.loaded = true;
                             });
                         }
@@ -38,6 +48,19 @@ define([
                 });
             };
 
+            $scope.connect = function() {
+                var pms = {
+                    imortId: $scope.imports[0].id,
+                    importProvider: $scope.imports[0].provider,
+                    yardi: {
+                        propertyId: $scope.pms.selectedProperty.id,
+                        floorplans: []
+                    }
+                };
+                console.log(pms);
+            };
+
             $scope.reload();
         }]);
+
 });
