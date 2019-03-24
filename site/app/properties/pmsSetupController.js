ffb10851b924959e2db5934864d3dfdfbf7c4020
Alex <alex@viderman.com>
2019-03-24 09:04:39 -0700

    configure pms import

---
+++ site/app/properties/pmsSetupController.js
@@ -0,0 +1,40 @@
+"use strict";
+define([
+    "app"
+], function(app) {
+    app.controller("pmsSetupController", ["$scope", "$uibModalInstance", "property", "ngProgress", "$propertyService", "$importService", "toastr", "$dialog", "$rootScope",
+        function($scope, $uibModalInstance, property, ngProgress, $propertyService, $importService, toastr, $dialog, $rootScope) {
+            ga("set", "title", "/pmsSetup");
+            ga("set", "page", "/pmsSetup");
+            ga("send", "pageview");
+
+            $scope.cancel = function() {
+                $uibModalInstance.dismiss("cancel");
+            };
+
+            $scope.reload = function() {
+                $scope.loaded = false;
+                $propertyService.search({
+                    limit: 1,
+                    permission: ["PropertyManage"],
+                    ids: [property._id],
+                    select: "_id floorplans orgid pms name",
+                }).then(function(response) {
+                    $scope.property = response.data.properties[0];
+                    // TODO: When this becomes client facing, we cannot return all integration client side
+                    $importService.read().then(function(response) {
+                        $scope.imports = _.filter(response.data, function(i) {
+                            return i.orgid.toString() === $scope.property.orgid.toString();
+                        });
+
+                        if ($scope.imports.length === 1) {
+                            // TODO: Get latest properties for import
+                        }
+                        $scope.loaded = true;
+                    });
+                });
+            };
+
+            $scope.reload();
+        }]);
+});
