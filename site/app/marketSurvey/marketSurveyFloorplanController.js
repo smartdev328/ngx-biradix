8c44bf2146fcdc81d012c21e708e9a1757d29109
MendimAvdyli <mendim.avdyli@gmail.com>
2019-04-19 20:53:51 +0200

    Floor Plan Pricing Breakdown design modification

--- site/app/marketSurvey/marketSurveyFloorplanController.js
+++ site/app/marketSurvey/marketSurveyFloorplanController.js
@@ -1,6 +1,7 @@
 angular.module("biradix.global").controller("marketSurveyFloorplanController", ["$scope", "$uibModalInstance", "floorplan",
     function($scope, $uibModalInstance, floorplan) {
         $scope.floorplan = floorplan;
+        console.log($scope.floorplan);
         $scope.cancel = function() {
             $uibModalInstance.dismiss("cancel");
         };
