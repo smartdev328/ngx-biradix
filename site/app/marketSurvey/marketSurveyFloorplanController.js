176dad54d08d2820f674b299cc9ef1001fdc5bd2
MendimAvdyli <mendim.avdyli@gmail.com>
2019-04-19 22:22:02 +0200

    Removed Floor Plan Info

--- site/app/marketSurvey/marketSurveyFloorplanController.js
+++ site/app/marketSurvey/marketSurveyFloorplanController.js
@@ -1,7 +1,6 @@
 angular.module("biradix.global").controller("marketSurveyFloorplanController", ["$scope", "$uibModalInstance", "floorplan",
     function($scope, $uibModalInstance, floorplan) {
         $scope.floorplan = floorplan;
-        console.log($scope.floorplan);
         $scope.cancel = function() {
             $uibModalInstance.dismiss("cancel");
         };
