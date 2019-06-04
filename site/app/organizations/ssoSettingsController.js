c08a89298813fb11f99018ce6ef7407924fb0451
Vitali Korepanov <vitali.korepanov@hugeturtle.com>
2019-06-04 17:32:40 +0300

    Feature: fake data for SSO configuration

--- site/app/organizations/ssoSettingsController.js
+++ site/app/organizations/ssoSettingsController.js
@@ -13,12 +13,29 @@ define([
             'ngProgress',
             '$rootScope',
             '$userService',
-            function ($scope, $uibModalInstance, organization, ngProgress, $rootScope, $userService) {
+            '$organizationsService',
+            'toastr',
+            function ($scope, $uibModalInstance, organization, ngProgress, $rootScope, $userService, $organizationsService, toastr) {
                 ga('set', 'title', '/ssoSettings');
                 ga('set', 'page', '/ssoSettings');
                 ga('send', 'pageview');
 
-                $scope.organization = organization;
+                // sso
+                $scope.SSOList = {};
+                $scope.SSOList.data = [
+                    {
+                        name: 'not use',
+                        id: undefined,
+                    },
+                    {
+                        name: 'Azure',
+                        id: 'azure',
+                    },
+                    {
+                        name: 'Okta',
+                        id: 'okta',
+                    },
+                ];
 
                 $scope.cancel = function () {
                     $uibModalInstance.dismiss('cancel');
@@ -48,46 +65,62 @@ define([
                 }
                 $scope.changeTab(0);
 
-                // sso
-                $scope.SSOList = {
-                    data: [
-                        {
-                            name: 'not use',
-                            id: undefined,
-                        },
-                        {
-                            name: 'Azure',
-                            id: 'azure',
-                        },
-                        {
-                            name: 'Okta',
-                            id: 'okta',
-                        },
-                    ],
-                    model: undefined,
-                    change: function () {
-                    },
-                };
-
-                // users
+                // load data
                 $scope.reload = function () {
-                    $userService.search({orgid: $scope.organization._id}).then(function(response) {
+                    $organizationsService.search().then(function (response) {
+                        $scope.organization = response.data.organizations.find((function (item, index) {
+                            return item._id == organization._id;
+                        }));
+                        $scope.SSOList.model = $scope.SSOList.data.find(function (item) {
+                            return item.id == $scope.organization.auth;
+                        });
+                    });
+                    $userService.search({orgid: organization._id}).then(function(response) {
                         $scope.userlist = response.data.users.map(function (user) {
                             return {
                                 id: user._id,
                                 name: `${user.name} (${user.email})`,
-                                selected: false,
+                                selected: user.sso,
                             };
                         });
                     });
                 };
+                //users list prepare
                 $scope.userlistOptions = {
                     availableLabel: 'Email-Password',
                     selectedLabel: 'SSO',
                     searchLabel: 'Email',
                 };
+
+                //call loading
                 $scope.reload();
 
+                $scope.save = function () {
+                    switch ($scope.tabIndex) {
+                        case 0:
+                            $scope.organization = {
+                                ...$scope.organization,
+                                auth: ($scope.SSOList.model || {id: undefined}).id,
+                            }
+                            $organizationsService.updateSSOSettings($scope.organization).then(function (response) {
+                                toastr.success(`SSO ${$scope.organization.name} Settings Updated Successfully`);
+                                ngProgress.complete();
+                            });
+                            break;
+                        case 1:
+                            var users = $scope.userlist.map(function (item, index) {
+                                return {
+                                    _id: item.id,
+                                    sso: item.selected,
+                                };
+                            });
+                            $userService.updateSSOSettings(users).then(function (response) {
+                                toastr.success(`SSO ${$scope.organization.name} Users' Settings Updated Successfully`);
+                                ngProgress.complete();
+                            });
+                            break;
+                    }
+                }
             }
         ]
     )
--- site/services/organizationsService.js
+++ site/services/organizationsService.js
@@ -1,6 +1,9 @@
 'use strict';
 angular.module('biradix.global').factory('$organizationsService', ['$http','$cookies', function ($http,$cookies) {
         var fac = {};
+        //emulation
+        fac.organizations = [];
+        //end emulation
 
         fac.public = function (subdomain) {
             return $http.get(gAPI + "/org/" + subdomain + '?bust=' + (new Date()).getTime(), {
@@ -14,6 +17,16 @@ angular.module('biradix.global').factory('$organizationsService', ['$http','$coo
         fac.search = function () {
             return $http.post(gAPI + '/api/1.0/organizations'+ '?bust=' + (new Date()).getTime(), {}, {
                 headers: {'Authorization': 'Bearer ' + $cookies.get('token') }}).success(function (response) {
+                //emulation
+                response.organizations = response.organizations.map(function (item, index) {
+                    return {
+                        ...item,
+                        ...fac.organizations.find(function (localItem, localIndex) {
+                            return localItem._id == item._id;
+                        })
+                    }
+                });
+                //end emulation
                 return response;
             }).error(function (response) {
                 return response;
@@ -28,6 +41,25 @@ angular.module('biradix.global').factory('$organizationsService', ['$http','$coo
                 return response;
             });
         }
+
+        //emulation
+        fac.updateSSOSettings = function (org) {
+            return new Promise(function (resolve, reject) {
+                var i = fac.organizations.findIndex(function (item, index) {
+                    return item._id == org._id;
+                });
+                if (i != -1) {
+                    fac.organizations[i] = {
+                        ...fac.organizations[i],
+                        ...org,
+                    };
+                } else {
+                    fac.organizations.push(org);
+                }
+                resolve(fac.organizations);
+            });
+        }
+        //end emulation
 
         return fac;
     }]);
