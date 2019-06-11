f6fb75935112d6f2958aa3fede4c78228b28962f
MendimAvdyli <mendim.avdyli@gmail.com>
2019-06-11 19:49:18 +0200

    Perspectives ui changes

--- site/app/excluded/excludedController.js
+++ site/app/excluded/excludedController.js
@@ -3,7 +3,7 @@ angular.module("biradix.global").controller("excludedController", ["$scope", "$u
         $scope.appliedPerspective = appliedPerspective;
         $scope.excludedList = excludedList;
         $scope.propertyId = propertyId;
-        $scope.title = $scope.appliedPerspective ? "Excluded Floor Plans" : "Missing Rent Values Floor Plans"
+        $scope.title = $scope.appliedPerspective ? "Excluded Floor Plans" : "Missing Rent Data Floor Plans"
         $scope.cancel = function() {
             $uibModalInstance.dismiss("cancel");
         };
