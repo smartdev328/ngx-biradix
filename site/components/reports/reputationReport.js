3861342bb058241ee5369d50e2fb9209c31dfa6b
MendimAvdyli <mendim.avdyli@gmail.com>
2019-06-13 00:18:17 +0200

    Yelp extra logic for decimal numbers 2

--- site/components/reports/reputationReport.js
+++ site/components/reports/reputationReport.js
@@ -15,16 +15,16 @@ angular.module("biradix.global").directive("reputationReport", function() {
                 el.triggerHandler("click");
             };
 
-            var yelpNumber = $scope.report.averages.yelpRatingAverage.toString().split(".")[0];
-            var yelpDecimal = $scope.report.averages.yelpRatingAverage.toString().split(".")[1];
-            if(parseInt(yelpDecimal) < 30) {
+            var yelpNumber = $scope.report.averages.yelpRatingAverage.toFixed(1).toString().split(".")[0];
+            var yelpDecimal = $scope.report.averages.yelpRatingAverage.toFixed(1).toString().split(".")[1];
+            if(parseInt(yelpDecimal) < 3) {
                 $scope.report.averages.yelpRatingAverage = parseInt(yelpNumber);
-            } else if(parseInt(yelpDecimal) > 70) {
+            } else if(parseInt(yelpDecimal) > 7) {
                 $scope.report.averages.yelpRatingAverage = parseInt(yelpNumber) + 1;
             } else {
                 $scope.report.averages.yelpRatingAverage = parseInt(yelpNumber) + 0.5;
             }
-            
+
         },
         templateUrl: "/components/reports/reputationReport.html?bust=" + version,
     };
