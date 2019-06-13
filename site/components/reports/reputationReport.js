c3112f68528bdc5be00382d07834cd8c0d42a505
MendimAvdyli <mendim.avdyli@gmail.com>
2019-06-13 18:42:23 +0200

    Updated yelp averages

--- site/components/reports/reputationReport.js
+++ site/components/reports/reputationReport.js
@@ -15,15 +15,28 @@ angular.module("biradix.global").directive("reputationReport", function() {
                 el.triggerHandler("click");
             };
 
-            var yelpNumber = $scope.report.averages.yelpRatingAverage.toFixed(1).toString().split(".")[0];
-            var yelpDecimal = $scope.report.averages.yelpRatingAverage.toFixed(1).toString().split(".")[1];
-            if(parseInt(yelpDecimal) < 3) {
-                $scope.report.averages.yelpRatingAverage = parseInt(yelpNumber);
-            } else if(parseInt(yelpDecimal) > 7) {
-                $scope.report.averages.yelpRatingAverage = parseInt(yelpNumber) + 1;
-            } else {
-                $scope.report.averages.yelpRatingAverage = parseInt(yelpNumber) + 0.5;
-            }
+            $scope.$watch('report.averages.yelpRatingAverage', function(newvalue) {
+                newvalue = parseFloat(newvalue.toFixed(1));
+                if(0.5 <= newvalue && newvalue <= 1.2) {
+                    $scope.report.averages.yelpRatingAverage = 1;
+                } else if(1.3 <= newvalue && newvalue <= 1.7) {
+                    $scope.report.averages.yelpRatingAverage = 1.5;
+                } else if(1.8 <= newvalue && newvalue <= 2.2) {
+                    $scope.report.averages.yelpRatingAverage = 2;
+                } else if(2.3 <= newvalue && newvalue <= 2.7) {
+                    $scope.report.averages.yelpRatingAverage = 2.5;
+                } else if(2.8 <= newvalue && newvalue <= 3.2) {
+                    $scope.report.averages.yelpRatingAverage = 3;
+                } else if(3.3 <= newvalue && newvalue <= 3.7) {
+                    $scope.report.averages.yelpRatingAverage = 3.5;
+                } else if(3.8 <= newvalue && newvalue <= 4.2) {
+                    $scope.report.averages.yelpRatingAverage = 4;
+                } else if(4.3 <= newvalue && newvalue <= 4.7) {
+                    $scope.report.averages.yelpRatingAverage = 4.5;
+                } else if(4.8 <= newvalue && newvalue <= 5) {
+                    $scope.report.averages.yelpRatingAverage = 5;
+                }
+            });
 
         },
         templateUrl: "/components/reports/reputationReport.html?bust=" + version,
