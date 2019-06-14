52cb770a55e1972c5a68263a46623f9bf09e7d36
MendimAvdyli <mendim.avdyli@gmail.com>
2019-06-14 20:14:20 +0200

    Yelp average update

--- site/components/reports/reputationReport.js
+++ site/components/reports/reputationReport.js
@@ -16,25 +16,9 @@ angular.module("biradix.global").directive("reputationReport", function() {
             };
 
             if($scope.report.averages.yelpRatingAverage) {
-                $scope.report.averages.yelpRatingAverage = parseFloat($scope.report.averages.yelpRatingAverage.toFixed(1));
-                if(0.5 <= $scope.report.averages.yelpRatingAverage && $scope.report.averages.yelpRatingAverage <= 1.2) {
-                    $scope.report.averages.yelpRatingAverage = 1;
-                } else if(1.3 <= $scope.report.averages.yelpRatingAverage && $scope.report.averages.yelpRatingAverage <= 1.7) {
-                    $scope.report.averages.yelpRatingAverage = 1.5;
-                } else if(1.8 <= $scope.report.averages.yelpRatingAverage && $scope.report.averages.yelpRatingAverage <= 2.2) {
-                    $scope.report.averages.yelpRatingAverage = 2;
-                } else if(2.3 <= $scope.report.averages.yelpRatingAverage && $scope.report.averages.yelpRatingAverage <= 2.7) {
-                    $scope.report.averages.yelpRatingAverage = 2.5;
-                } else if(2.8 <= $scope.report.averages.yelpRatingAverage && $scope.report.averages.yelpRatingAverage <= 3.2) {
-                    $scope.report.averages.yelpRatingAverage = 3;
-                } else if(3.3 <= $scope.report.averages.yelpRatingAverage && $scope.report.averages.yelpRatingAverage <= 3.7) {
-                    $scope.report.averages.yelpRatingAverage = 3.5;
-                } else if(3.8 <= $scope.report.averages.yelpRatingAverage && $scope.report.averages.yelpRatingAverage <= 4.2) {
-                    $scope.report.averages.yelpRatingAverage = 4;
-                } else if(4.3 <= $scope.report.averages.yelpRatingAverage && $scope.report.averages.yelpRatingAverage <= 4.7) {
-                    $scope.report.averages.yelpRatingAverage = 4.5;
-                } else if(4.8 <= $scope.report.averages.yelpRatingAverage && $scope.report.averages.yelpRatingAverage <= 5) {
-                    $scope.report.averages.yelpRatingAverage = 5;
+                $scope.report.averages.yelpRatingAverage = (Math.round($scope.report.averages.yelpRatingAverage * 2) / 2).toFixed(1);
+                if(parseFloat($scope.report.averages.yelpRatingAverage) <= 0.5) {
+                    $scope.report.averages.yelpRatingAverage = 0;
                 }
             }
 
