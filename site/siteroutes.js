a8eb1e279266af086c51c43f05f21a3b94ceb0e0
Alex Viderman <alex@viderman.com>
2019-07-13 13:07:32 -0700

    NP-1527 Unable to add CDN in front of platform

--- site/siteroutes.js
+++ site/siteroutes.js
@@ -80,6 +80,7 @@ module.exports = (function() {
     });
 
     ui.get("/sso", function(req, res) {
+        echo(req.query);
         res.redirect('/#/login?r=' + encodeURIComponent(req.query.r) + "&t=" + encodeURIComponent(req.query.token));
    });
 
