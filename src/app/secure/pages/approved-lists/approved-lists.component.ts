7091cd1b2e5b9321c8bb9e9646985f6c3ba09479
Alex Viderman <alex@viderman.com>
2019-08-09 15:51:35 -0700

    Approved Lists Filters

--- src/app/secure/pages/approved-lists/approved-lists.component.ts
+++ src/app/secure/pages/approved-lists/approved-lists.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort} from '@angular/material';
-import { FormControl } from '@angular/forms';
 import {MatTableDataSource} from '@angular/material/table';
 import {ApprovedListsService, PerformanceService} from "../../../core/services";
 import {APPROVED_LIST_TYPE, APPROVED_LIST_LABELS, IApprovedListItemRead} from "../../../core/models/approvedLists";
@@ -16,7 +15,7 @@ export class ApprovedListsComponent implements OnInit {
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  typeFilter = new FormControl(APPROVED_LIST_TYPE.OWNER);
+  typeFilter: APPROVED_LIST_TYPE = APPROVED_LIST_TYPE.OWNER;
   valueFilter: string;
   fields: any;
 
@@ -28,7 +27,7 @@ export class ApprovedListsComponent implements OnInit {
               private performanceService: PerformanceService) {
   }
 
-  fieldFilterFunction() {
+  filterBySearch() {
     this.dataSource.filter = this.valueFilter.trim().toLowerCase();
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
@@ -37,30 +36,26 @@ export class ApprovedListsComponent implements OnInit {
 
   async ngOnInit() {
     this.performanceService.start();
-    this.fields = APPROVED_LIST_LABELS;
 
+    this.fields = APPROVED_LIST_LABELS;
     this.dataSource = new MatTableDataSource();
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
 
+    await this.run();
+
+    this.performanceService.fireGoogleAnalytics('Approved Lists');
+  }
+
+  async run() {
     const approvedLists: IApprovedListItemRead[] = await this.approvedListsService.searchApproved({
       limit: 10000,
-      type: APPROVED_LIST_TYPE.OWNER,
+      type: this.typeFilter,
       searchableOnly: false
     });
 
     this.dataSource.data = approvedLists;
-
-    this.typeFilter.valueChanges.subscribe(async (typeFilterValue) => {
-      const approvedLists: IApprovedListItemRead[] = await this.approvedListsService.searchApproved({
-        limit: 10000,
-        type: typeFilterValue,
-        searchableOnly: false
-      });
-      this.dataSource.data = approvedLists;
-    });
-
-    this.performanceService.fireGoogleAnalytics('Approved Lists');
+    this.filterBySearch();
   }
 
 }
