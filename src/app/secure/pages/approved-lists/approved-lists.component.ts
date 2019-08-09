03a54e324f739812f4afabcb9ab23683e52427b3
MendimAvdyli <mendim.avdyli@gmail.com>
2019-08-09 14:59:48 +0200

    Fixed the field dropdown

--- src/app/secure/pages/approved-lists/approved-lists.component.ts
+++ src/app/secure/pages/approved-lists/approved-lists.component.ts
@@ -5,35 +5,6 @@ import {MatTableDataSource} from '@angular/material/table';
 import {ApprovedListsService, PerformanceService} from "../../../core/services";
 import {APPROVED_LIST_TYPE, APPROVED_LIST_LABELS, IApprovedListItemRead} from "../../../core/models/approvedLists";
 
-export interface IApprovedListItemReadExample {
-  value: string;
-  type: APPROVED_LIST_TYPE;
-  searchable: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: IApprovedListItemReadExample[] = [
-  { type: APPROVED_LIST_TYPE.OWNER, value: 'Hydrogen', searchable: 1.0079, symbol: 'H' },
-  { type: APPROVED_LIST_TYPE.OWNER, value: 'Helium', searchable: 4.0026, symbol: 'He' },
-  { type: APPROVED_LIST_TYPE.MANAGER, value: 'Lithium', searchable: 6.941, symbol: 'Li' },
-  { type: APPROVED_LIST_TYPE.OWNER, value: 'Beryllium', searchable: 9.0122, symbol: 'Be' },
-  { type: APPROVED_LIST_TYPE.FEES, value: 'Boron', searchable: 10.811, symbol: 'B' },
-  { type: APPROVED_LIST_TYPE.MANAGER, value: 'Carbon', searchable: 12.0107, symbol: 'C' },
-  { type: APPROVED_LIST_TYPE.FEES, value: 'Nitrogen', searchable: 14.0067, symbol: 'N' },
-  { type: APPROVED_LIST_TYPE.OWNER, value: 'Oxygen', searchable: 15.9994, symbol: 'O' },
-  { type: APPROVED_LIST_TYPE.MANAGER, value: 'Fluorine', searchable: 18.9984, symbol: 'F' },
-  { type: APPROVED_LIST_TYPE.FEES, value: 'Neon', searchable: 20.1797, symbol: 'Ne' },
-  { type: APPROVED_LIST_TYPE.OWNER, value: 'Helium', searchable: 4.0026, symbol: 'He' },
-  { type: APPROVED_LIST_TYPE.MANAGER, value: 'Lithium', searchable: 6.941, symbol: 'Li' },
-  { type: APPROVED_LIST_TYPE.OWNER, value: 'Beryllium', searchable: 9.0122, symbol: 'Be' },
-  { type: APPROVED_LIST_TYPE.FEES, value: 'Boron', searchable: 10.811, symbol: 'B' },
-  { type: APPROVED_LIST_TYPE.MANAGER, value: 'Carbon', searchable: 12.0107, symbol: 'C' },
-  { type: APPROVED_LIST_TYPE.FEES, value: 'Nitrogen', searchable: 14.0067, symbol: 'N' },
-  { type: APPROVED_LIST_TYPE.OWNER, value: 'Oxygen', searchable: 15.9994, symbol: 'O' },
-  { type: APPROVED_LIST_TYPE.MANAGER, value: 'Fluorine', searchable: 18.9984, symbol: 'F' },
-  { type: APPROVED_LIST_TYPE.FEES, value: 'Neon', searchable: 20.1797, symbol: 'Ne' },
-];
-
 @Component({
   selector: 'app-approved-lists',
   templateUrl: './approved-lists.component.html',
@@ -45,14 +16,9 @@ export class ApprovedListsComponent implements OnInit {
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  typeFilter = new FormControl();
-  valueFilter = new FormControl();
+  typeFilter = new FormControl(APPROVED_LIST_TYPE.OWNER);
+  valueFilter: string;
   fields: any;
-
-  filteredValues = {
-    type: '',
-    value: ''
-  };
 
   displayedColumns: string[] = ['value', 'type', 'searchable', 'delete'];
 
@@ -62,17 +28,8 @@ export class ApprovedListsComponent implements OnInit {
               private performanceService: PerformanceService) {
   }
 
-  customFilterPredicate() {
-    const myFilterPredicate = (data: IApprovedListItemRead, filter: string): boolean => {
-      let searchString = JSON.parse(filter);
-      return data.type.toString().trim().indexOf(searchString.type) !== -1 &&
-        data.value.toString().trim().toLowerCase().indexOf(searchString.value.toLowerCase()) !== -1;
-    }
-
-    return myFilterPredicate;
-  }
-
-  takeToFirstPage() {
+  fieldFilterFunction() {
+    this.dataSource.filter = this.valueFilter.trim().toLowerCase();
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
@@ -86,18 +43,6 @@ export class ApprovedListsComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
 
-    this.typeFilter.valueChanges.subscribe((typeFilterValue) => {
-      this.filteredValues['type'] = typeFilterValue;
-      this.dataSource.filter = JSON.stringify(this.filteredValues);
-    });
-
-    this.valueFilter.valueChanges.subscribe((valueFilterValue) => {
-      this.filteredValues['value'] = valueFilterValue;
-      this.dataSource.filter = JSON.stringify(this.filteredValues);
-    });
-
-    this.dataSource.filterPredicate = this.customFilterPredicate();
-
     const approvedLists: IApprovedListItemRead[] = await this.approvedListsService.searchApproved({
       limit: 10000,
       type: APPROVED_LIST_TYPE.OWNER,
@@ -105,6 +50,15 @@ export class ApprovedListsComponent implements OnInit {
     });
 
     this.dataSource.data = approvedLists;
+
+    this.typeFilter.valueChanges.subscribe(async (typeFilterValue) => {
+      const approvedLists: IApprovedListItemRead[] = await this.approvedListsService.searchApproved({
+        limit: 10000,
+        type: typeFilterValue,
+        searchableOnly: false
+      });
+      this.dataSource.data = approvedLists;
+    });
 
     this.performanceService.fireGoogleAnalytics('Approved Lists');
   }
